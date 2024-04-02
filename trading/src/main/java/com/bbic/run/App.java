27f547254fcb8f9ee30cd2143c6852f02b72df0c
Lucas-Choi-17 <csissis@naver.com>
2024-04-02 17:31:27 +0900

    feat: 프로그램 실행 main 메소드 생성

--- trading/src/main/java/com/bbic/run/App.java
+++ trading/src/main/java/com/bbic/run/App.java
@@ -1,8 +1,25 @@
 package com.bbic.run;
 
+import com.bbic.model.dto.User;
+import com.bbic.view.MainMenu;
+
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
-        
+
+        Scanner sc = new Scanner(System.in);
+
+        // User 정보 입력
+        System.out.println("사용자 정보를 입력하세요.");
+        System.out.print("이름 : ");
+        String name = sc.nextLine();
+        System.out.print("예수금 : ");
+        int deposit = sc.nextInt();
+
+        MainMenu mainMenu = new MainMenu(new User(name, deposit));
+
+        mainMenu.mainMenu();
     }
 }
