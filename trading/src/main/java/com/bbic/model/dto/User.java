c5b3e99019b169da6f0658e2d45a98b0ccceb783
Lucas-Choi-17 <csissis@naver.com>
2024-04-03 17:40:55 +0900

    feat: user 보유 주식 정보 출력 기능 구현

--- trading/src/main/java/com/bbic/model/dto/User.java
+++ trading/src/main/java/com/bbic/model/dto/User.java
@@ -1,6 +1,8 @@
 package com.bbic.model.dto;
 
 
+import java.util.ArrayList;
+
 public class User {
     //   1. 멤버
 //    1. 멤버 데이터
@@ -10,8 +12,10 @@ public class User {
 //            - 구매가격
 //            - 수량
 
+    //    private ArrayList<UserStockDTO> stocks = null;
+    public ArrayList<UserStockDTO> stocks = new ArrayList<>();
     private String name; //이름
-    private int  UserId ; //구매 가격
+    private int UserId; //구매 가격
     private int deposit; //예수금
 
     public User() {
@@ -21,6 +25,24 @@ public class User {
         this.name = name;
         UserId = userId;
         this.deposit = deposit;
+    }
+
+    public String showHoldingStocks() {
+        String holdingStockList = "";
+        for (UserStockDTO stock : stocks) {
+            holdingStockList += holdingStockInformation(stock);
+        }
+        return holdingStockList;
+    }
+
+    public String holdingStockInformation(UserStockDTO stock) {
+        String info = "";
+
+        info += "종목 : %-8s".formatted(stock.getStockData().getStockName()); // 이름
+        info += "수량 : %-5d".formatted(stock.getCount());                    // 보유 수량
+        info += "수익 : %-9d".formatted((stock.getStockData().getPrice() - stock.AveragePrice) * stock.getCount());     // 주당 변동량
+
+        return info + "\n";
     }
 
     public String getName() {
