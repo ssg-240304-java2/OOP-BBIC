8686c1d9238488d67c17d32f3bad1c4d4b2ef90d
Lucas-Choi-17 <csissis@naver.com>
2024-04-03 10:59:19 +0900

    feat: StockManager 생성

--- trading/src/main/java/com/bbic/controller/StockManager.java
+++ trading/src/main/java/com/bbic/controller/StockManager.java
@@ -1,4 +1,53 @@
 package com.bbic.controller;
 
+import java.util.Scanner;
+
 public class StockManager {
+
+    // 주식 list
+
+    // 매수 class
+
+    // 매도 class
+
+
+    public StockManager() {
+
+    }
+
+    public void sellOrBuy() {
+
+        int stockId = selectStock();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("""
+                원하시는 거래를 선택하세요. 매수(1) / 매도(2)
+                매수 / 매도 :\s""");
+        int stockOption = sc.nextInt();
+
+        switch (stockOption) {
+            case 1:
+                // 매수 method <- (User, stockId, stockOption)
+                break;
+            case 2:
+                // 매도 method <- (User, stockId, stockOption)
+                break;
+        }
+    }
+
+    public int selectStock() {
+        Scanner sc = new Scanner(System.in);
+        // 종목 출력
+        this.showAllStockPrice();
+        System.out.println("""
+                종목을 선택하세요.
+                ---------------------
+                종목 코드 :\s""");
+        return sc.nextInt();
+    }
+
+
+    public void showAllStockPrice() {
+
+    }
 }
